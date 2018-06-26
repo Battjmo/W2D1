a23a0266bd422c8e1f723d1a9879138f02687ccc
nickbattjes <nhbattjes@gmail.com>
2018-06-25 18:03:54 -0700

    Implement starting chess classes

---
+++ chess/board.rb
@@ -0,0 +1,50 @@
+require_relative "piece"
+
+class Board
+  def initialize
+    @grid = Array.new(8) {Array.new(8)}
+    populate
+  end 
+  
+  def populate
+    pieces = [0, 1, 6, 7]
+    pieces.each do |row| # 0
+      @grid[row].map!{|el| el = Piece.new}
+    end 
+  end 
+  
+  def move_piece(start_pos, end_pos)
+    start_row, start_col = start_pos
+    end_row, end_col = end_pos
+    [start_row, start_col, end_row, end_col].each do |n|
+      raise "Invalid position" unless n.between?(0,7)
+    end
+    if self[start_pos] == nil
+      raise "No piece at this position."
+    else
+      self[end_pos] = self[start_pos]
+      self[start_pos] = nil
+    end
+      
+    #RAISE ERROR IF NOTHING IN STARTING POSITION
+    #RAISE ERROR IF END POS IS AN INVALID MOVE 
+  end 
+  
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end 
+  
+  def []=(pos, piece)
+    row, col = pos
+    @grid[row][col] = piece
+  end
+  
+  
+
+  
+  
+  
+  
+#END OF CLASS
+end 
---
+++ chess/piece.rb
@@ -0,0 +1,7 @@
+class Piece 
+  def initialize
+    @name = "piece"
+  end 
+
+
+end 
